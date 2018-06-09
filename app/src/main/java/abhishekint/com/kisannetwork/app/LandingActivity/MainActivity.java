7ad1f2306970d818648309a309200687309e3397
Abhishek Kumar <abhishekint16@gmail.com>
2018-06-09 15:21:53 +0530

    changing project str

--- app/src/main/java/abhishekint/com/kisannetwork/app/LandingActivity/MainActivity.java
+++ app/src/main/java/abhishekint/com/kisannetwork/app/LandingActivity/MainActivity.java
@@ -1,4 +1,4 @@
-package abhishekint.com.kisannetwork;
+package abhishekint.com.kisannetwork.app.LandingActivity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -6,6 +6,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import abhishekint.com.kisannetwork.R;
 import butterknife.BindView;
 
 /**
