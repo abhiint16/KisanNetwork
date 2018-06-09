ce4232bd8bfc6549b2f826f5b432a53c7d83b42a
Abhishek Kumar <abhishekint16@gmail.com>
2018-06-09 13:56:25 +0530

    init project

---
+++ app/src/main/java/abhishekint/com/kisannetwork/MainActivity.java
@@ -0,0 +1,10 @@
+package abhishekint.com.kisannetwork;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by abhishek on 09-06-2018.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
