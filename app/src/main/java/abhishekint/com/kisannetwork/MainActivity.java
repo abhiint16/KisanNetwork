17619da5e78ef49147efc9751517ccf5f77a440d
Abhishek Kumar <abhishekint16@gmail.com>
2018-06-09 15:17:45 +0530

    adding tablayout and viewpager

--- app/src/main/java/abhishekint/com/kisannetwork/MainActivity.java
+++ app/src/main/java/abhishekint/com/kisannetwork/MainActivity.java
@@ -2,13 +2,21 @@ package abhishekint.com.kisannetwork;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+
+import butterknife.BindView;
 
 /**
  * Created by abhishek on 09-06-2018.
  */
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.main_activity_tab)
+    TabLayout tabLayout;
+    @BindView(R.id.main_activity_viewpager)
+    ViewPager viewPager;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
